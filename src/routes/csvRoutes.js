44b356c5062871feb66c0d43641decfd8a19d416
Diego Lauar <diego_pktd@hotmail.com>
2025-02-12 14:12:32 -0300

    Adicionando atualizacao de task

--- src/routes/csvRoutes.js
+++ src/routes/csvRoutes.js
@@ -29,7 +29,7 @@ router.post('/task', upload.single('file'), (req, res) => {
         .on('end', () => {
             fs.unlinkSync(filePath); // Remover o CSV temporário
             const updatedData = saveJson(jsonFileName, results); // Salvar os dados no JSON
-            
+
             res.json({ message: 'Arquivo processado e salvo com sucesso!', data: updatedData });
         })
         .on('error', (err) => {
@@ -38,7 +38,7 @@ router.post('/task', upload.single('file'), (req, res) => {
 });
 
 router.get('/task', (req, res) => {
-    const filename  ='/dados.json';
+    const filename = '/dados.json';
     const filePath = path.join(JSON_DIR, filename);
 
     if (!fs.existsSync(filePath)) {
@@ -52,7 +52,7 @@ router.get('/task', (req, res) => {
 });
 
 router.get('/task/:id', (req, res) => {
-    const filename  ='/dados.json';
+    const filename = '/dados.json';
     const filePath = path.join(JSON_DIR, filename);
 
     if (!fs.existsSync(filePath)) {
@@ -60,12 +60,56 @@ router.get('/task/:id', (req, res) => {
     }
 
     const fileContent = fs.readFileSync(filePath, 'utf-8');
-    let jsonData = JSON.parse(fileContent);
-    const result = jsonData.filter((e)=> e.id == req.params.id)
-    console.log(result)
+    const jsonData = JSON.parse(fileContent);
+    const result = jsonData.filter((e) => e.id == req.params.id)
+
+    if (result.length == 0) {
+        return res.status(400).json({ error: 'Nenhum id encontrado!' });
+    }
+
 
     res.json({ message: 'Dados carregados com sucesso!', data: result });
 });
+
+router.put('/task/:id', (req, res) => {
+
+    const { id } = req.params;
+    const novosDados = req.body;
+
+    if (!req.body.title && !req.body.description) {
+        return res.status(404).json({ error: 'Nenhum dado para atualizar!' });
+    }
+
+    const filename = '/dados.json';
+    const filePath = path.join(JSON_DIR, filename);
+
+    if (!fs.existsSync(filePath)) {
+        return res.status(404).json({ error: 'Arquivo JSON não encontrado!' });
+    }
+
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    let task = JSON.parse(fileContent);
+    let taskAtualizada
+    // Atualiza a task do usuário
+    task = task.map(task => {
+        if (task.id === id) {
+            taskAtualizada = { ...task, ...novosDados };
+            return taskAtualizada;
+        }
+        return task;
+    });
+
+    if (!taskAtualizada) {
+        return res.status(404).json({ error: 'Task não encontrado' });
+    }
+
+    fs.writeFile(filePath, JSON.stringify(task, null, 2), 'utf-8', (err) => {
+        if (err) return res.status(500).json({ error: 'Erro ao salvar o arquivo' });
+        res.json({ message: 'Idade atualizada com sucesso!', data: taskAtualizada });
+    });
+
+});
+
 
 
 
